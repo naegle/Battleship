a16c4e86ff6df22062fc595a2f1ef93f5c36bf60
Tom Nguyen <nguyentom1996@yahoo.com>
2019-11-12 14:29:58 -0700

    Solution framework for Battleship

---
+++ Battleship/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Battleship.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
