f7e94fbaa7abee15d19bc18bd8386c1d4c349661
Tom Nguyen <nguyentom1996@yahoo.com>
2019-11-16 14:48:30 -0700

    Setted up the DB

---
+++ Battleship/Data/DbInitializer.cs
@@ -0,0 +1,18 @@
+﻿using Battleship.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Battleship.Data
+{
+    public class DbInitializer
+    {
+        public static void Initialize(BattleshipDBContext context)
+        {
+            context.Database.Migrate();
+
+        }
+    }
+}
