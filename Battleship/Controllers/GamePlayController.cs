67b5a9e060957dc6fa468e03deb870e96f0fc8b6
Tom Nguyen <nguyentom1996@yahoo.com>
2019-11-25 00:48:48 -0700

    There is now a clickable grid for us to use

---
+++ Battleship/Controllers/GamePlayController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Battleship.Controllers
+{
+    public class GamePlayController : Controller
+    {
+        public IActionResult Index()
+        {
+            
+           // return View("GamePlayViewPage");
+            return View("GamePlayViewPage");
+        }
+    }
+}
