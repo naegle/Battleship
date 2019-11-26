205aa8d819bc49d5a44f512526c582deb2eef2df
Tom Nguyen <nguyentom1996@yahoo.com>
2019-11-25 20:46:41 -0700

    Trying different kind of grids

--- Battleship/wwwroot/js/grid.js
+++ Battleship/wwwroot/js/grid.js
@@ -58,8 +58,51 @@ var column = row.selectAll(".square")
     .on('click', function (d) {
         d.click++;
         if ((d.click) % 4 == 0) { d3.select(this).style("fill", "#0xFF0000"); }
-        if ((d.click) % 4 == 1) { d3.select(this).style("fill", "#2C93E8"); }
+        if ((d.click) % 4 == 1) { d3.select(this).style("fill", "#808080"); }
         if ((d.click) % 4 == 2) { d3.select(this).style("fill", "#F56C4E"); }
         if ((d.click) % 4 == 3) { d3.select(this).style("fill", "#ffffff"); }
     });
 
+
+// function that builds a grid in the "container"
+function createGrid(x) {
+    for (var rows = 0; rows < x; rows++) {
+        for (var columns = 0; columns < x; columns++) {
+            $("#container").append("<div class='grid'></div>");
+        };
+    };
+    $(".grid").width(256 / x);
+    $(".grid").height(256 / x);
+};
+
+// function that clears the grid
+function clearGrid() {
+    $(".grid").remove();
+};
+
+// function that prompts the user to select the number of boxes in a new grid
+// the function then also creates that new grid
+function refreshGrid() {
+    var z = prompt("How many boxes per side?");
+    clearGrid();
+    createGrid(z);
+};
+
+// create a 16x16 grid when the page loads
+// creates a hover effect that changes the color of a square to black when the mouse passes over it, leaving a (pixel) trail through the grid
+// allows the click of a button to prompt the user to create a new grid
+$(document).ready(function () {
+    createGrid(10);
+
+    $(".grid").mouseover(function () {
+        $(this).css("background-color", "#808080");
+    });
+
+    $(".newGrid").click(function () {
+        refreshGrid();
+
+        $(".grid").mouseover(function () {
+            $(this).css("background-color", "black");
+        });
+    });
+});
