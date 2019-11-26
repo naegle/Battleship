76025065ea5a3b03415acc843fc5854cb5b30984
Tom Nguyen <nguyentom1996@yahoo.com>
2019-11-25 23:18:44 -0700

    Now there is a player grid and AI grid Now you can get the x y of the current cell When hover on AI, the cursor will change to a aim

--- Battleship/wwwroot/js/grid.js
+++ Battleship/wwwroot/js/grid.js
@@ -1,78 +1,18 @@
-﻿function gridData() {
-    var data = new Array();
-    var xpos = 1; //starting xpos and ypos at 1 so the stroke will show when we make the grid below
-    var ypos = 1;
-    var width = 50;
-    var height = 50;
-    var click = 0;
-
-    // iterate for rows	
-    for (var row = 0; row < 10; row++) {
-        data.push(new Array());
-
-        // iterate for cells/columns inside rows
-        for (var column = 0; column < 10; column++) {
-            data[row].push({
-                x: xpos,
-                y: ypos,
-                width: width,
-                height: height,
-                click: click
-            })
-            // increment the x position. I.e. move it over by 50 (width variable)
-            xpos += width;
-        }
-        // reset the x position after a row is complete
-        xpos = 1;
-        // increment the y position for the next row. Move it down 50 (height variable)
-        ypos += height;
-    }
-    return data;
-}
-
-var gridData = gridData();
-
-// I like to log the data to the console for quick debugging
-console.log(gridData);
-
-var grid = d3.select("#grid")
-    .append("svg")
-    .attr("width", "510px")
-    .attr("height", "510px");
-
-var row = grid.selectAll(".row")
-    .data(gridData)
-    .enter().append("g")
-    .attr("class", "row");
-
-var column = row.selectAll(".square")
-    .data(function (d) { return d; })
-    .enter().append("rect")
-    .attr("class", "square")
-    .attr("x", function (d) { return d.x; })
-    .attr("y", function (d) { return d.y; })
-    .attr("width", function (d) { return d.width; })
-    .attr("height", function (d) { return d.height; })
-    .style("fill", "#fff")
-    .style("stroke", "#222")
-    .on('click', function (d) {
-        d.click++;
-        if ((d.click) % 4 == 0) { d3.select(this).style("fill", "#0xFF0000"); }
-        if ((d.click) % 4 == 1) { d3.select(this).style("fill", "#808080"); }
-        if ((d.click) % 4 == 2) { d3.select(this).style("fill", "#F56C4E"); }
-        if ((d.click) % 4 == 3) { d3.select(this).style("fill", "#ffffff"); }
-    });
-
-
+﻿
 // function that builds a grid in the "container"
 function createGrid(x) {
     for (var rows = 0; rows < x; rows++) {
         for (var columns = 0; columns < x; columns++) {
-            $("#container").append("<div class='grid'></div>");
+            $("#playerGrid").append("<div class='Player_Cell' " + "id=" + rows + "_" + columns + " x=" + rows + " y=" + columns + "></div>");
+            $("#AIGrid").append("<div class='AI_Cell' " + "id=" + rows + "_" + columns + " x=" + rows + " y=" + columns + "></div>");
         };
     };
-    $(".grid").width(256 / x);
-    $(".grid").height(256 / x);
+    $(".Player_Cell").width(500 / x);
+    $(".Player_Cell").height(500 / x);
+
+    $(".AI_Cell").width(500 / x);
+    $(".AI_Cell").height(500 / x);
+    $(".AI_Cell").mouseover = function () { mouseHoverAI() };
 };
 
 // function that clears the grid
@@ -92,17 +32,12 @@ function refreshGrid() {
 // creates a hover effect that changes the color of a square to black when the mouse passes over it, leaving a (pixel) trail through the grid
 // allows the click of a button to prompt the user to create a new grid
 $(document).ready(function () {
+
     createGrid(10);
 
-    $(".grid").mouseover(function () {
-        $(this).css("background-color", "#808080");
-    });
+    //$(".grid").mouseover(function () {
+    //    $(this).css("background-color", "#808080");
+    //});
 
-    $(".newGrid").click(function () {
-        refreshGrid();
-
-        $(".grid").mouseover(function () {
-            $(this).css("background-color", "black");
-        });
-    });
 });
+
