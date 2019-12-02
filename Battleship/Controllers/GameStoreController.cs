366ecfb066a1779a5c3cb68d0de7c072e171d5c1
Tom Nguyen <nguyentom1996@yahoo.com>
2019-12-02 15:58:11 -0700

    User information can be access when accesing the store page. Able to get the user credit, quantity count for each power

--- Battleship/Controllers/GameStoreController.cs
+++ Battleship/Controllers/GameStoreController.cs
@@ -2,14 +2,62 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Battleship.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Battleship.Controllers
 {
     public class GameStoreController : Controller
     {
+        private readonly UserManager<IdentityUser> userManager;
+        private readonly BattleshipDBContext battleshipContext;
+
+        public GameStoreController(UserManager<IdentityUser> _userManager, BattleshipDBContext _battshipContext)
+        {
+            userManager = _userManager;
+            battleshipContext = _battshipContext;
+        }
+
         public IActionResult Index()
         {
+
+            // check if user is in the db
+            string loggedUsername = userManager.GetUserName(User);
+
+            var databaseUser = battleshipContext.Inventories.Where(i => i.PlayerId == loggedUsername).FirstOrDefault();
+
+            // if empty
+            if (databaseUser == null)
+            {
+                // then create one 
+
+                Inventory tempInventory = new Inventory();
+                tempInventory.PlayerId = loggedUsername;
+                tempInventory.Power1 = 0;
+                tempInventory.Power2 = 0;
+                tempInventory.Power3 = 0;
+                tempInventory.Cash = 0;
+
+                battleshipContext.Add(tempInventory);
+                battleshipContext.SaveChangesAsync();
+
+                ViewData["UserCredit"] = 0;
+                ViewData["Power1Count"] = 0;
+                ViewData["Power2Count"] = 0;
+                ViewData["Power3Count"] = 0;
+
+
+            }
+            else
+            {
+                ViewData["UserCredit"] = databaseUser.Cash;
+                ViewData["Power1Count"] = databaseUser.Power1;
+                ViewData["Power2Count"] = databaseUser.Power2;
+                ViewData["Power3Count"] = databaseUser.Power3;
+            }
+
+
             return View();
         }
 
--- Battleship/wwwroot/js/Store.js
+++ Battleship/wwwroot/js/Store.js
@@ -1,7 +1,9 @@
 ﻿function purchase_power1() {
 
-    document.getElementById("playerIncome").style.color = "red";
-  
+    //do a swal fire to update the use credit
+    // and quanity count
+    // if user try to buy the power but don't have enough credit 
+    // a second swal fire telling the user you don't have enough credit
 }
 
 
